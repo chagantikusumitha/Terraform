ab54a84a8dd76af2a1bbf61185a0ef2bdae9b352
chagantikusumitha <92007497+chagantikusumitha@users.noreply.github.com>
2024-04-01 16:28:12 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,11 @@
 # Networking (VPC Setup)
 # main.tf
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+default = ""
+}
+variable "aws_secret_key" {
+default = ""
+}
 variable "aws_region" {
 default = "us-east-1"
 }
